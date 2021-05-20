882606f51fcb30100150aebe228fb13fd695adc6
haki9975 <elias.robert.hakim@gmail.com>
2021-05-20 13:41:52 -0400

    commit tree 4513c33e6bf6a647a0d500cbca11ad8ede78af28

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
+///multiply(num1, num2)
+random()
 
-// Write your code here
+//function multiply(num1, num2 ){
+//    console.log( num1 * num2)
+//    return num1 * num2
+// };
+
+function random() {
+ return  Math.floor(Math.random() *10 ) +1
+}
